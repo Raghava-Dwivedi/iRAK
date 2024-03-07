d31b4c9cc8f79f69375429564788930ef93ddd68
Raghava-Dwivedi <raghava.p.dwivedi@gmail.com>
2024-03-07 10:14:21 -0600

    Initial Commit

---
+++ iRAK/iRAKApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRAKApp.swift
+//  iRAK
+//
+//  Created by 64016641 on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iRAKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
